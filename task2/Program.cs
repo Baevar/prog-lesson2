2cd33c2f5b77d9b4e1f5e2e82b8348e5982779ed
baevar_laptop <baevar@mai.ru>
2024-03-15 09:41:03 +0700

    add lesson2:task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int N, i;
+Console.WriteLine("Введите число N");
+N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+for (i = 0; i < N; i++)
+{
+    arr[i]=i+1;
+    Console.Write("{0} ",arr[i]);
+}
