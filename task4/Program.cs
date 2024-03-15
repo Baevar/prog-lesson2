2efeb2a64b81a782e4e2f67033648dab9b3be2a1
baevar_laptop <baevar@mai.ru>
2024-03-15 10:38:32 +0700

    lesson02:task4_red

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,11 +1,31 @@
 ﻿int[] arr = { 24, 8, 12, 11, 7, 4, 16, 4, 2, 33 };
 int i = 1, max = arr[0];
-while (i <arr.Length)
+while (i < arr.Length)
 {
-    if (arr[i]>max)
+    if (arr[i] > max)
     {
-        max= arr[i];
+        max = arr[i];
     }
     i++;
 }
-Console.WriteLine($"max={max}");
+Console.WriteLine($"max(while)={max}");
+
+max = arr[0];
+for (int j = 1; j < arr.Length; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.WriteLine($"max(for)={max}");
+max = arr[0];
+
+foreach (int k in arr)
+{
+    if (k > max)
+    {
+        max = k;
+    }
+}
+Console.WriteLine($"max(foreach)={max}");
