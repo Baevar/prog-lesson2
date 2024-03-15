bf0d0622f38cecd34e81e76d6c37e197fdd9f508
baevar_laptop <baevar@mai.ru>
2024-03-15 10:07:17 +0700

    add lesson2:task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = { 24, 8, 12, 11, 7, 4, 16, 4, 2, 33 };
+int i = 1, max = arr[0];
+while (i <arr.Length)
+{
+    if (arr[i]>max)
+    {
+        max= arr[i];
+    }
+    i++;
+}
+Console.WriteLine($"max={max}");
